ee1af633a3a473252e0cabbc16d850e62812d480
HARRISKING <llc13588440410@outlook.com>
2018-06-25 17:13:41 +0800

    css file add success

--- mi-page/src/index.js
+++ mi-page/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './view/Home';
 
+import './style/base.css'
+
 ReactDOM.render(
     <Home />, 
     document.getElementById('root')
