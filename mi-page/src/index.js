74f5175b96668c8d9eb6826d3512b9c2055a9b54
HARRISKING <llc13588440410@outlook.com>
2018-06-27 16:19:02 +0800

    finally finish the cli

--- mi-page/src/index.js
+++ mi-page/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './views/Home.jsx'
 
+import "./style/home.scss";
+import "./style/test.css"
+
 ReactDOM.render(
     <Home />,
     document.getElementById('root')
--- mi-page/webpack.config.js
+++ mi-page/webpack.config.js
@@ -26,14 +26,21 @@ module.exports = {
                 test: /(\.jsx|\.js)$/,
                 use: {
                     loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            'env','react'
-                        ]
-                    }
                 },
                 exclude: /node_modules/
 
+            },
+            {
+                test: /(\.css|\.scss)$/,
+                use: [
+                    {
+                        loader:"style-loader"
+                    },{
+                        loader:"css-loader"
+                    },{
+                        loader:"sass-loader"
+                    }
+                ]
             }
         ]
     },
